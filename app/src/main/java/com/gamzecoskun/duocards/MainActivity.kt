b23ae07a9282e292ab51430d3f10d5c7da252b81
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-09-10 21:59:15 +0300

    Implement MainActivity with navigation and BottomNavigationView setup

--- app/src/main/java/com/gamzecoskun/duocards/MainActivity.kt
+++ app/src/main/java/com/gamzecoskun/duocards/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.gamzecoskun.duocards
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.gamzecoskun.duocards.common.viewBinding
+import com.gamzecoskun.duocards.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
+    private lateinit var navController: NavController
+
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+
+        val navHostController = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostController.navController
+        binding.bottomNav.setupWithNavController(navController)
+
+        bottomNav=binding.bottomNav
+
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.wordListFragment -> {
+                    navController.navigate(R.id.wordListFragment)
+                    true
+                }
+                R.id.learnedWordsFragment -> {
+                    navController.navigate(R.id.learnedWordsFragment)
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
