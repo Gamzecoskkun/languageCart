ee8fb23756f7780ad6271ec98991dd167801a0ef
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-09-13 13:15:41 +0300

    configure Hilt with DuoCards application class

---
+++ app/src/main/java/com/gamzecoskun/duocards/di/DuoCards.kt
@@ -0,0 +1,12 @@
+package com.gamzecoskun.duocards.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/***
+ * Created on 12.09.2024
+ *@author Gamze Coşkun
+ */
+
+@HiltAndroidApp
+class DuoCards:Application()
