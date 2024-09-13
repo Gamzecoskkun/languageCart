d1cd6a784ed6bb54f9692971d9285544370457f6
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-09-13 22:58:13 +0300

    Add Splash

---
+++ app/src/main/java/com/gamzecoskun/duocards/ui/Splash/SplashFragment.kt
@@ -0,0 +1,23 @@
+package com.gamzecoskun.duocards.ui.Splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gamzecoskun.duocards.R
+import com.gamzecoskun.duocards.common.viewBinding
+import com.gamzecoskun.duocards.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    private val binding by viewBinding(FragmentSplashBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+}
