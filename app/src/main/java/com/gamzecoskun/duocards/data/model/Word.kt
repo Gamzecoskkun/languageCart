f634fc2f310921e5664137a89eb18f19ae0d59a1
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-09-13 13:28:37 +0300

    resolve duplicate property name and update Word data class

--- app/src/main/java/com/gamzecoskun/duocards/data/model/Word.kt
+++ app/src/main/java/com/gamzecoskun/duocards/data/model/Word.kt
@@ -1,13 +1,11 @@
-package com.gamzecoskun.duocards
+package com.gamzecoskun.duocards.data.model
 
 import java.io.Serializable
-import java.util.concurrent.atomic.AtomicInteger
 
 
 data class Word(
-    val uuid:Int,
     val englishWord: String,
     val turkishWord: String,
-    val imageResId: Int,
+    val imageResId: String,
     var isLearned: Boolean = false,
 ) : Serializable
